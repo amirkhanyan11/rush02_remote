4ac9adc10018b78a62ba4aa465d4a4aa7f09308f
amirkhanyan11 <138108895+amirkhanyan11@users.noreply.github.com>
2023-12-03 22:34:19 +0400

    finish

--- main.c
+++ main.c
@@ -1,70 +1,37 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aamirkha <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/02 15:41:41 by aamirkha          #+#    #+#             */
-/*   Updated: 2023/12/02 18:25:45 by aamirkha         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
-#include <fcntl.h>
 #include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
-#include "__algorithms__.h"
-//#define DEBUG
+#include "__strstr__.h"
+#include "checking.h"
+#include "print_2.h"
+#include "print.h"
 
-char	*stringify(int exp)
+int main(int argc, char** argv)  
 {
-	char	*nb;
-	char	*t_nb;
-
-	nb = (char*)malloc (exp + 1);
-	nb[0] = '1';
-	t_nb = nb + 1;
-	while (exp >= 1)
-	{
-		*t_nb = '0';
-		t_nb++;
-		exp--;
-	}
-#ifdef DEBUG
-	printf("%s\n", nb);
-#endif
-	return (nb);
-}
-
-void get_it(char* buffer, int exp)
-{
-	char	*needle;
-	char	*needle_print;
-
-	needle = stringify(exp);
-	needle_print = ft_strstr(buffer, needle);
-	while (is_alpha(*needle_print))
-	{
-		write(1, needle_print, 1);
-		needle_print++;
-	}
-#ifdef DEBUG
-	printf("%s\n", needle_print);
-#endif
-	write(1, "\n", 1);
-	free(needle);
-}
-
-int main()
-{
-	
-	int fd = open("numbers.dict", O_RDONLY);
-	char buffer[600];
-	read(fd, buffer, 500);
-	//printf("%s\n", buffer);
-	close(fd);
-	
-	get_it(buffer, 4);	
-	return 0;
+    if (argc == 1 || argc > 3)
+    {
+        write(1,"Error", 5);
+        return 1;
+    }
+    if (argc == 2)
+    {
+        if (check_num_arg(argv[1]))
+        {
+            return 1;
+        }
+    }
+    check_and_swap_args(argv);
+    if (argv[1][0] == '0')
+    {
+        write(1,"Error", 5);
+        return 1;
+    }
+    int fd;
+    char buffer[1000];
+    fd = open(argv[2], O_RDONLY);
+    read(fd, buffer, 1000);    
+    print_nume_name(argv[1], buffer, stlen(argv[1]));
+    close(fd);
+    return 0;
 }
